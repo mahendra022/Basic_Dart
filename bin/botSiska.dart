592c5c74c2ece2bf437b22fa0afc08970a52be50
Mahendra022 <ekamahendra86@gmail.com>
2021-07-20 21:59:00 +0700

    Make telegram Bot Siska

---
+++ bin/botSiska.dart
@@ -0,0 +1,16 @@
+import 'package:teledart/teledart.dart';
+import 'package:teledart/telegram.dart';
+
+void main() {
+  var teledart = TeleDart(
+      Telegram('1910138470:AAHLaQ15pgIqUmmm61C5hE7j7GO1jTVsVNU'), Event());
+
+  teledart.start().then((me) => print('${me.username} is initialised'));
+
+  teledart
+      .onCommand('hello')
+      .listen(((message) => teledart.replyMessage(message, 'world')));
+  teledart
+      .onCommand('dart')
+      .listen(((message) => teledart.replyMessage(message, 'Flutter')));
+}
